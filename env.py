008f4ce3b07e9ff6388d0cc70d28fb33d3b161e9
Bradley Martin <twinsen43@B-Rad-2.local>
2016-08-27 14:50:46 -0400

    removing

--- env.py
+++
@@ -1,9 +0,0 @@
-env = {
-    "SECRET_KEY": "y__qv6u8cg8-j@*z7%%@5_1sedb@!w9$d4r!73j&)hf%d4d*48",
-    "DATABASE_PASS": "Nothing FOr NOw",
-    "DB_NAME": "None existing",
-    "DB_USER": "None",
-    "DB_PASSWORD": "",
-    "EMAIL_HOST_USER": "twinaen9@gmail.com",
-    "EMAIL_HOST_PASSWORD": "",
-}
